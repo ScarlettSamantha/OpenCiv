85a21c09163b3e2e2bd454be4b92a2fe69ce1032
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-08-15 23:42:19 +0200

    :sparkles: add effects exception.

---
+++ openciv/engine/exceptions/effects_exception.py
@@ -0,0 +1,21 @@
+from openciv.engine.exceptions.gameplay_exception import GameplayException
+
+
+class EffectException(GameplayException):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class EffectDoesNotExist(EffectException):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class EffectAlreadyExists(EffectException):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class EffectCannotBeBoughtOff(EffectException):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
