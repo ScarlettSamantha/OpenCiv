e8d432427a107e95ee4cc9cb1ffbfe8325b0cd66
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-08-15 23:32:55 +0200

    🐛 fix callbacks not triggering in citizins

--- openciv/gameplay/citizens.py
+++ openciv/gameplay/citizens.py
@@ -1,7 +1,7 @@
 from openciv.gameplay.citizen import Citizen
 from openciv.engine.mixins.callbacks import CallbacksMixin
 
-from typing import List
+from typing import Any, List, Self
 
 
 class Citizens(CallbacksMixin):
@@ -10,36 +10,36 @@ class Citizens(CallbacksMixin):
         self._citizens: List[Citizen] = []
         self.register_citizen_events()
 
-    def register_citizen_events(self):
-        self._declare_event("on_birth")
+    def register_citizen_events(self) -> None:
+        self._declare_event(event="on_birth")
 
-    def add(self, value: Citizen, as_birth: bool = True):
+    def add(self, value: Citizen, as_birth: bool = True) -> None:
         self._citizens.append(value)
         if as_birth:
-            super(CallbacksMixin).trigger_callback("on_birth", value)
+            self.trigger_all_callbacks(category="on_birth", citizen=value)
 
-    def remove(self, value: Citizen | int = 1, as_death: bool = True):
+    def remove(self, value: Citizen | int = 1, as_death: bool = True) -> None:
         if isinstance(value, int):
-            for i in range(value):
+            for _ in range(value):
                 self._citizens.pop()
-        elif isinstance(value, Citizen):
+        else:
             self._citizens.remove(value)
 
         if as_death:
-            super(CallbacksMixin).trigger_callback("on_death", value)
+            self.trigger_all_callbacks(category="on_death", citizen=value)
 
-    def reset(self):
+    def reset(self) -> None:
         self._citizens: List[Citizen] = []
 
-    def create(self, num: int = 1, *args, **kwargs):
-        def _create(self, *args, **kwargs):
-            self.add(Citizen(*args, **kwargs))
+    def create(self, num: int = 1, *args: Any, **kwargs: Any) -> None:
+        def _create(self: Self, *args: Any, **kwargs: Any) -> None:
+            self.add(value=Citizen(*args, **kwargs))
 
-        for i in range(num):
-            _create(self, *args, **kwargs)
+        for _ in range(num):
+            _create(self=self, *args, **kwargs)
 
     def __add__(self, value: Citizen):
-        self.add(value)
+        self.add(value=value)
 
     def __len__(self) -> int:
         return len(self._citizens)
