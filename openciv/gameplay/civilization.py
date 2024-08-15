9c1fe474031ce80fead6e04ecb6258d2650b9d7f
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-08-15 23:33:32 +0200

    :sparkles: add some mor eof te civilization skeleton

--- openciv/gameplay/civilization.py
+++ openciv/gameplay/civilization.py
@@ -1,14 +1,16 @@
-from openciv.gameplay.effects import Effects
+from typing import Self, List
+from openciv.gameplay.effect import Effects, Effect
+from openciv.gameplay.leader import Leader
 from abc import abstractmethod
 
 
 class Civilization:
-    def __init__(self, name: str, description: str):
+    def __init__(self, name: str, description: str) -> None:
         self.name: str = name
-        self.icon: str = None
+        self.icon: str | None = None
         self._loadable = False
         self.description: str = description
-        self.leaders: list = []
+        self._leaders: List[Leader] = []
 
         self._effects: Effects = Effects()
 
@@ -16,29 +18,37 @@ class Civilization:
         self.register_effects()
         self.register_leaders()
 
-    def effects(self):
+    def effects(self) -> Effects:
         return self._effects
 
-    def add_effect(self, effect):
-        self._effects.add(effect, effect.name)
+    def add_effect(self, effect: Effect) -> None:
+        self._effects.add(effect=effect, key_or_auto=str(effect.name))
 
-    def add_leader(self, leader):
+    def add_leader(self, leader: Leader) -> None:
         self.leaders.append(leader)
 
-    def leaders(self):
-        return self.leaders
+    @property
+    def leaders(self) -> List[Leader]:
+        return self._leaders
+
+    @leaders.setter
+    def leaders(self, leaders: List[Leader]) -> None:
+        self._leaders = leaders
 
     @abstractmethod
-    def register_effects(self):
+    def register_effects(self) -> None:
         pass
 
     @abstractmethod
-    def register_leaders(Self):
+    def register_leaders(Self) -> None:
         pass
 
-    def __str__(self):
-        leaders = ", ".join([leader.name for leader in self.leaders])
+    def __str__(self) -> str:
+        leader_name_list: List[str] = []
+        for leader in self.leaders:
+            leader_name_list.append(leader.name if leader.name else "Unknown")
+        leaders: str = ", ".join(leader_name_list)
         return f"{self.name} - {self.description} <{leaders}>"
 
-    def __call__(self):
+    def __call__(self) -> Self:
         return self
