30a4340469ff760f554f4fce87c935ddb85e37a5
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-08-15 23:31:04 +0200

    :sparkles: add civic and culture requirement object aliases

--- openciv/engine/requires.py
+++ openciv/engine/requires.py
@@ -1,4 +1,6 @@
-from typing import List, Any, ForwardRef
+from __future__ import annotations
+
+from typing import List, Any, Union, Iterable
 from openciv.engine.exceptions.condition_exception import ConditionObjectPropertyDoesNotExist
 from openciv.engine.saving import SaveAble
 from openciv.engine.mixins.callbacks import CallbacksMixin
@@ -6,11 +8,11 @@ from collections.abc import Callable
 
 
 class Condition(SaveAble, CallbacksMixin):
-    def __init__(self, obj: object, property: str, required_value: Any, *args, **kwargs) -> None:
+    def __init__(self, obj: object, property: str, required_value: Any, *args: Any, **kwargs: Any) -> None:
         SaveAble.__init__(self, *args, **kwargs)
         CallbacksMixin.__init__(self, *args, **kwargs)
 
-        self.obj: obj = obj
+        self.obj: object = obj
         self.property: str = property
         self.required_value = required_value
 
@@ -25,7 +27,7 @@ class Condition(SaveAble, CallbacksMixin):
         _property_ref = getattr(self.obj, self.property)
         # If the property is a function, call it and compare the result to the required value
         if isinstance(_property_ref, Callable):
-            return _property_ref() == self.required_value
+            return _property_ref() == self.required_value  # type: ignore
 
         return _property_ref == self.required_value
 
@@ -34,7 +36,7 @@ class Condition(SaveAble, CallbacksMixin):
 
 
 class ConditionMultiple(Condition):
-    def __init__(self, conditions: List[Condition], *args, **kwargs) -> None:
+    def __init__(self, conditions: List[Condition], *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.conditions: List[Condition] = conditions
 
@@ -46,40 +48,75 @@ class ConditionMultiple(Condition):
 
 
 class Requires(Condition):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
 
 
 class RequiresMultiple(ConditionMultiple):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
+
+    @classmethod
+    def convert_from_list(cls, conditions: T_Requires) -> RequiresMultiple:
+        return cls(conditions=conditions)
 
 
 class RequiresCivicComplete(Requires):
-    def __init__(self, civic: ForwardRef("Civic"), *args, **kwargs):
+    def __init__(self, civic: Civic, *args: Any, **kwargs: Any):  # type: ignore
         super().__init__(obj=civic, property="completed", required_value=True, *args, **kwargs)
 
 
 class RequiresCivicsComplete(RequiresMultiple):
-    def __init__(self, civics: List[ForwardRef("Civic")], *args, **kwargs):
+    def __init__(self, civics: List[Civic], *args: Any, **kwargs: Any):  # type: ignore
         super().__init__(*args, **kwargs)
-        for civic in civics:
-            if not isinstance(civic, ForwardRef("Civic")):
-                raise TypeError(f"RequiresCivicsComplete requires a list of Civics, not {type(civic)}")
-            _instance: RequiresCivicComplete = RequiresCivicComplete(civic)
+        for civic in civics:  # type: ignore
+            _instance: RequiresCivicComplete = RequiresCivicComplete(civic=civic)  # type: ignore
             self.conditions.append(_instance)
 
 
 class RequriesSubtreeCompelete(Requires):
-    def __init__(self, subtree: ForwardRef("CultureSubtree"), *args, **kwargs):
+    def __init__(self, subtree: CultureSubtree, *args: Any, **kwargs: Any) -> None:  # type: ignore
         super().__init__(obj=subtree, property="is_completed", required_value=True, *args, **kwargs)
 
 
 class RequiresSubtreesComplete(RequiresMultiple):
-    def __init__(self, subtrees: List[ForwardRef("CultureSubtree")], *args, **kwargs):
+    def __init__(self, subtrees: List[CultureSubtree], *args: Any, **kwargs: Any):  # type: ignore
         super().__init__(*args, **kwargs)
-        for subtree in subtrees:
-            if not isinstance(subtree, ForwardRef("CultureSubtree")):
-                raise TypeError(f"RequiresSubtreesComplete requires a list of CultureSubtrees, not {type(subtree)}")
-            _instance: RequriesSubtreeCompelete = RequriesSubtreeCompelete(subtree)
+        for subtree in subtrees:  # type: ignore
+            _instance: RequriesSubtreeCompelete = RequriesSubtreeCompelete(subtree=subtree)  # type: ignore
             self.conditions.append(_instance)
+
+
+class RequiresPromotionComplete(Requires):
+    def __init__(self, promotion: Promotion, *args: Any, **kwargs: Any):  # type: ignore
+        super().__init__(obj=promotion, property="aquired", required_value=True, *args, **kwargs)
+
+
+class RequiresPromotionsComplete(RequiresMultiple):
+    def __init__(self, promotions: List[Promotion], *args: Any, **kwargs: Any) -> None:  # type: ignore
+        super().__init__(*args, **kwargs)
+        for promotion in promotions:  # type: ignore
+            _instance: RequiresPromotionComplete = RequiresPromotionComplete(promotion=promotion)  # type: ignore
+            self.conditions.append(_instance)
+
+
+class RequiresPromotionTreeUnlocked(Requires):
+    def __init__(self, promotion_tree: PromotionTree, *args: Any, **kwargs: Any) -> None:  # type: ignore
+        super().__init__(obj=promotion_tree, property="unlocked", required_value=True, *args, **kwargs)
+
+
+class RequiresPromotionTreesUnlocked(RequiresMultiple):
+    def __init__(self, promotion_trees: List[PromotionTree], *args: Any, **kwargs: Any) -> None:  # type: ignore
+        super().__init__(*args, **kwargs)
+        for promotion_tree in promotion_trees:  # type: ignore
+            _instance: RequiresPromotionTreeUnlocked = RequiresPromotionTreeUnlocked(promotion_tree=promotion_tree)  # type: ignore
+            self.conditions.append(_instance)
+
+
+T_Requires = Union[Requires, RequiresMultiple, Iterable[Requires], None]
+
+# Forward references without import
+Civic = "Civic"
+CultureSubtree = "CultureSubtree"
+Promotion = "Promotion"
+PromotionTree = "PromotionTree"
