9db163f1ffa555d2fb68915e238d845b224865b4
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-08-15 23:25:31 +0200

    👷 fix typing of singleton

--- openciv/engine/mixins/singleton.py
+++ openciv/engine/mixins/singleton.py
@@ -1,30 +1,32 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
-from typing import Type, TypeVar, Optional
+from typing import Type, TypeVar, Optional, Any, cast
 
 T = TypeVar("T", bound="Singleton")
 
 
 class Singleton(ABC):
-    __instance: Optional[T] = None
+    __instance: Optional[Singleton] = None
 
     @abstractmethod
-    def __setup__(self, *args, **kwargs) -> None:
+    def __setup__(self, *args: Any, **kwargs: Any) -> None:
         """Method to set up the singleton instance."""
         pass
 
-    def __new__(cls: Type[T], *args, **kwargs) -> T:
-        if cls.__instance is None:
-            cls.__instance = super(Singleton, cls).__new__(cls)
+    def __new__(cls: Type[T], *args: Any, **kwargs: Any) -> T:
+        if cls.__instance is None or not isinstance(cls.__instance, cls):
+            cls.__instance = super(Singleton, cls).__new__(cls)  # type: ignore
             cls.__instance.__setup__(*args, **kwargs)
-        return cls.__instance
+        return cast(T, cls.__instance)
 
     @classmethod
-    def _get_instance(cls: Type[T], *args, **kwargs) -> T:
-        if cls.__instance is None:
+    def get_instance(cls: Type[T], *args: Any, **kwargs: Any) -> T:
+        if cls.__instance is None or not isinstance(cls.__instance, cls):
             cls.__instance = cls.__new__(cls, *args, **kwargs)
-        return cls.__instance
+        return cast(T, cls.__instance)
 
     @classmethod
     def _set_instance(cls: Type[T], instance: T) -> T:
         cls.__instance = instance
-        return cls.__instance
+        return instance
