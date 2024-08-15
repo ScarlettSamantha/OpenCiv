7cc4120864114688872a4805a09d1b7301326863
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-08-15 23:28:18 +0200

    :sparkles: basic cities object skeleton

--- openciv/gameplay/cities.py
+++ openciv/gameplay/cities.py
@@ -1,21 +1,24 @@
 from openciv.gameplay.city import City
+from typing import List, Iterator
 
 
 class Cities:
-    def __init__(self):
-        self._cities = []
+    def __init__(self) -> None:
+        self._cities: List[City] = []
 
-    def add(self, value: City):
+    def add(self, value: City) -> None:
         self._cities.append(value)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[City]:
         self.index = 0
+        return self
 
     def __len__(self) -> int:
         return len(self._cities)
 
     def __next__(self) -> City:
-        if self.index >= self.__len__():
+        if self.index >= len(self._cities):
             raise StopIteration
+        city: City = self._cities[self.index]
         self.index += 1
-        return self._cities[self.index]
+        return city
