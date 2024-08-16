f03763284fafadc0ae1c7d7995faaf7e204b8472
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-08-16 10:04:08 +0200

    :sparkles: make leader object saveable, and register properties.

--- openciv/gameplay/leader.py
+++ openciv/gameplay/leader.py
@@ -1,17 +1,22 @@
+from __future__ import annotations
+
 from openciv.gameplay.effect import Effects, Effect
 from openciv.engine.managers.i18n import T_TranslationOrStr
+from openciv.engine.saving import SaveAble
 
 
-class Leader:
+class Leader(SaveAble):
     def __init__(
         self, key: str, name: T_TranslationOrStr, description: T_TranslationOrStr, icon: str | None = None
     ) -> None:
+        super().__init__()
         self.key: str = key
         self.name: T_TranslationOrStr = name
         self.icon: str | None = icon
         self.description: T_TranslationOrStr = description
 
         self._effects: Effects = Effects()
+        self._setup_saveable()
 
     @property
     def effects(self) -> Effects:
