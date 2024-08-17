fa7276749fb063d12e9679e2871b9e197adc57b9
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2024-08-17 21:57:00 +0200

    🏷️  fix typing of resource exception

--- openciv/engine/exceptions/resource_exception.py
+++ openciv/engine/exceptions/resource_exception.py
@@ -1,16 +1,19 @@
+from __future__ import annotations
+
 from openciv.engine.exceptions._base_exception import BaseException
+from typing import Any
 
 
 class ResourceException(BaseException):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)  # type: ignore
 
 
 class ResourceTypeException(ResourceException):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
 
 class ResourceMathException(ResourceException):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
